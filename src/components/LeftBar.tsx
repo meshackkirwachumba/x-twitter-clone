2a67d0072c906fdb0a85fb2396bd382e0677b4b8
meshackkirwachumba <meshackkirwachumba@gmail.com>
2025-09-13 13:33:03 +0300

    set up breakpoints

---
+++ src/components/LeftBar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LeftBar = () => {
+  return <div>left bar</div>;
+};
+
+export default LeftBar;
---
+++ src/components/RightBar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RightBar = () => {
+  return <div>right bar</div>;
+};
+
+export default RightBar;
